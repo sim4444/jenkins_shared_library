712351ef317bb126d34789bbaa81cf7c564456a2
sim44 <skaur617@my.bcit.ca>
2025-04-08 09:18:30 -0700

    Force Jenkins to refresh shared library

--- vars/cicdpipeline.groovy
+++ vars/cicdpipeline.groovy
@@ -43,11 +43,11 @@ def call(Map config) {
                 }
                 steps {
                     dir("${config.serviceDir}") {
-                        withCredentials([usernamePassword(credentialsId: 'DockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_TOKEN')]) {
-                            sh "echo $DOCKER_TOKEN | docker login -u $DOCKER_USER --password-stdin"
-                            sh "docker build -t ${config.imageName}:latest --tag ${config.imageName}:${config.tag} ."
-                            sh "docker push ${config.imageName}:${config.tag}"
-                        }
+                        withCredentials([usernamePassword(credentialsId: 'DockerHubSim', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_TOKEN')]) {
+                        sh "echo $DOCKER_TOKEN | docker login -u $DOCKER_USER --password-stdin"
+                        sh "docker build -t ${config.imageName}:latest --tag ${config.imageName}:${config.tag} ."
+                        sh "docker push ${config.imageName}:${config.tag}"
+                    }
                     }
                 }
             }
